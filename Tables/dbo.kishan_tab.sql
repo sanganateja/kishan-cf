0e8f1658ff7ab62e4d1a8376ddb55838d1a7562a
Ravi <sangana575@gmail.com>
2020-07-17 11:17:04 +0530

    this is the mytable

---
+++ Tables/dbo.kishan_tab.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[kishan_tab]
+(
+[sno] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[names] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[empid] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO

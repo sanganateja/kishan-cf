a4d8efcc33ee0bd5099bfe3249618a7c98768b69
Ravi <sangana575@gmail.com>
2020-07-24 17:58:02 +0530

    Added new coloumn

---
+++ tech/Migrations/1.0.0-Baseline/001_20200724-1755_11201441.sql
@@ -0,0 +1,13 @@
+﻿-- <Migration ID="ae6aae23-6101-4997-8ab3-569e432b0192" />
+GO
+
+PRINT N'Creating [dbo].[kishan_tab]'
+GO
+CREATE TABLE [dbo].[kishan_tab]
+(
+[sno] [nchar] (10) NULL,
+[names] [nchar] (10) NULL,
+[empid] [nchar] (10) NULL,
+[my] [nchar] (10) NULL
+)
+GO
---
+++ tech/Schema-Model/dbo/Tables/kishan_tab.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[kishan_tab]
+(
+[sno] [nchar] (10) NULL,
+[names] [nchar] (10) NULL,
+[empid] [nchar] (10) NULL,
+[my] [nchar] (10) NULL
+)
+GO

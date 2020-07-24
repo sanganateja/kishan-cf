11a4644fc2d3612e3b90525fe4c1776508f473e7
Ravi <sangana575@gmail.com>
2020-07-24 18:19:47 +0530

    Added new table kis2

---
+++ tech/Migrations/1.1.0-Changes/001_20200724-1818_11201441.sql
@@ -0,0 +1,12 @@
+﻿-- <Migration ID="7af67aef-8d3b-4206-a61d-ec17f759eb25" />
+GO
+
+PRINT N'Creating [dbo].[kis2]'
+GO
+CREATE TABLE [dbo].[kis2]
+(
+[sno] [nchar] (10) NULL,
+[names] [nchar] (10) NULL,
+[salary] [nchar] (10) NULL
+)
+GO
---
+++ tech/Schema-Model/dbo/Tables/kis2.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[kis2]
+(
+[sno] [nchar] (10) NULL,
+[names] [nchar] (10) NULL,
+[salary] [nchar] (10) NULL
+)
+GO
